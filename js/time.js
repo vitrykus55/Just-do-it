5f9fba943e519666731cfce787081c964d46d815
Oleksandr Vitrykus <oleksandr.vitrykus@hicmedia.com>
2025-03-24 18:02:22 +0100

    Add timer and animation for main text, also create time.js file.

---
+++ js/time.js
@@ -0,0 +1,12 @@
+function updateClock() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth();
+    const day = now.getDate();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    document.getElementById('clock').innerHTML = `<span class="time-setup">${day}/${month}/${year}, ${hours}:${minutes}</span>`;
+}
+
+setInterval(updateClock, 10000);
+updateClock();
