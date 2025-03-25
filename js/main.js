d5ec4b7038032b12e9b6f60e5a802faf5b094862
Oleksandr Vitrykus <oleksandr.vitrykus@hicmedia.com>
2025-03-25 19:06:31 +0100

    Add accepted task and Deleted Task rebuild js.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,57 @@
 function addTask() {
+    const taskCollection = document.getElementById('collection');
+    const taskInput = document.getElementById('task');
 
-    const sendData = document.getElementById("send-data");
-    let taskCollection = document.getElementById('collection');
-
-    if (!sendData || !taskCollection) {
+    if (!taskInput || !taskCollection) {
+        console.warn('Data is missing.');
+        return;
+    }
+    const dataTask = taskInput.value.trim();
+    if (!dataTask) {
         console.warn('Data is missing.');
         return;
     }
 
-    sendData.addEventListener("click", () => {
-        const dataTask = document.getElementById("task").value.trim();
 
-        if (!dataTask) {
-            console.warn('Data is missing.');
-            return;
-        }
-        const newTask = document.createElement("div");
-        newTask.classList.add("task");
-        newTask.innerHTML = `
-            <div class="text-to-do">
-                <span>${dataTask}</span>
-            </div>
-            <div class="main-buttom">
-                <img src="svg/check.svg" alt="img">
-                <img src="svg/trash.svg" alt="img">
-            </div>
-        `;
+    const newTask = document.createElement("div");
+    newTask.classList.add("task");
+    newTask.innerHTML = `
+    <div class="text-to-do">
+        <span>${dataTask}</span>
+    </div>
+    <div class="main-buttom">
+        <img src="svg/check.svg" alt="img" data-target="accept">
+        <img src="svg/trash.svg" alt="img" data-target="delete">
+    </div>
+    `;
 
-        taskCollection.appendChild(newTask);
 
-        document.getElementById("task").value = "";
-
-    });
+    taskCollection.appendChild(newTask);
+    document.getElementById("task").value = "";
 
 }
 
-addTask()
+function wholeElement() {
+    const taskCollection = document.getElementById('collection');
+
+    taskCollection.addEventListener("click", (event) => {
+        const target = event.target.dataset.target;
+
+        if (target === 'accept') {
+            console.log("Accepted Task");
+            event.target.closest(".task").classList.add("completed");
+        } else if (target === 'delete') {
+            console.log("Deleted Task");
+            event.target.closest(".task").remove();
+        }
+    });
+}
+
+
+function startTask() {
+    document.getElementById('send-data').addEventListener('click', addTask);
+    wholeElement();
+}
+
+
+startTask();
